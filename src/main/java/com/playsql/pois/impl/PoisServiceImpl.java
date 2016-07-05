cdf61526e2080c66e4e798c4b5b7ea51e18b6b5e
Adrien Ragot <adrien.ragot@gmail.com>
2016-07-05 15:36:01 +0200

    RY-74 Make Play SQL Exports open-source

--- src/main/java/com/playsql/pois/impl/PoisServiceImpl.java
+++ src/main/java/com/playsql/pois/impl/PoisServiceImpl.java
@@ -1,5 +1,25 @@
 package com.playsql.pois.impl;
 
+/*
+ * #%L
+ * Play SQL Exports
+ * %%
+ * Copyright (C) 2016 Play SQL S.A.S.U.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.playsql.pois.api.PoisService;
 import com.playsql.pois.api.WorkbookAPI;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
