fc68a4d1f0717c14fa7358912b3b391ac2b2f6e2
BLU Eli <eli@play-sql.com>
2019-03-11 17:31:25 +0100

    RY-263 Unused code removed

--- src/main/java/com/playsql/psea/impl/PseaServiceImpl.java
+++ src/main/java/com/playsql/psea/impl/PseaServiceImpl.java
@@ -23,28 +23,16 @@ package com.playsql.psea.impl;
 import com.google.common.collect.Lists;
 import com.playsql.psea.api.*;
 import com.playsql.psea.utils.Utils.Clock;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.poi.ss.formula.eval.NotImplementedException;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.util.SAXHelper;
-import org.apache.poi.xssf.model.SharedStringsTable;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.xml.sax.Attributes;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
 
-import javax.annotation.Nonnull;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
@@ -198,66 +186,6 @@ public class PseaServiceImpl implements PseaService {
             cellValue = cell.getCellFormula();
         }
         return (cellValue == null) ? null : Objects.toString(cellValue);
-    }
-
-
-    public XMLReader fetchSheetParser(SharedStringsTable sst) throws SAXException, ParserConfigurationException {
-        XMLReader parser = SAXHelper.newXMLReader();
-        ContentHandler handler = new SheetHandler(sst);
-        parser.setContentHandler(handler);
-        return parser;
-    }
-
-    /**
-     * See org.xml.sax.helpers.DefaultHandler javadocs
-     */
-    private static class SheetHandler extends DefaultHandler {
-        private SharedStringsTable sst;
-        private String lastContents;
-        private boolean nextIsString;
-
-        private SheetHandler(SharedStringsTable sst) {
-            this.sst = sst;
-        }
-
-        public void startElement(String uri, String localName, String name,
-                                 Attributes attributes) throws SAXException {
-            // c => cell
-            if(name.equals("c")) {
-                // Print the cell reference
-                System.out.print(attributes.getValue("r") + " - ");
-                // Figure out if the value is an index in the SST
-                String cellType = attributes.getValue("t");
-                if(cellType != null && cellType.equals("s")) {
-                    nextIsString = true;
-                } else {
-                    nextIsString = false;
-                }
-            }
-            // Clear contents cache
-            lastContents = "";
-        }
-
-        public void endElement(String uri, String localName, String name)
-                throws SAXException {
-            // Process the last contents as required.
-            // Do now, as characters() may be called more than once
-            if(nextIsString) {
-                int idx = Integer.parseInt(lastContents);
-                lastContents = sst.getEntryAt(idx).toString();
-                nextIsString = false;
-            }
-
-            // v => contents of a cell
-            // Output after we've seen the string contents
-            if(name.equals("v")) {
-                System.out.println(lastContents);
-            }
-        }
-
-        public void characters(char[] ch, int start, int length) {
-            lastContents += new String(ch, start, length);
-        }
     }
 
 }
