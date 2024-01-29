35737d2cf20b74371b956cb8cf489a61c95280db
Adrien Ragot <aragot@r-yogi.com>
2024-01-29 21:22:38 +0100

    PSEA-25 Fix an API, which, for reasons I can't understand, keeps passing a small-int instead of an object Integer

--- src/main/java/com/playsql/psea/api/ExcelImportConsumer.java
+++ src/main/java/com/playsql/psea/api/ExcelImportConsumer.java
@@ -33,7 +33,7 @@ public abstract class ExcelImportConsumer {
     private final int maxRecordsPerTransaction;
 
     /**
-     * @deprecated Use {@link #ExcelImportConsumer(Integer, int, String, Integer, List)}
+     * @deprecated Use {@link #ExcelImportConsumer(Integer, Integer, String, Integer, List)}
      */
     public ExcelImportConsumer(Integer maxRows, String focusedSheet, Integer focusedRow, List<String> inactiveSheets) {
         this.maxRows = maxRows;
@@ -43,7 +43,8 @@ public abstract class ExcelImportConsumer {
         this.maxRecordsPerTransaction = 1000;
     }
 
-    public ExcelImportConsumer(Integer maxRows, int maxRecordsPerTransaction, String focusedSheet, Integer focusedRow, List<String> inactiveSheets) {
+    /** In 1.9.4, changed the second parameter back from int to Integer, because it caused an error on production (and not in development) */
+    public ExcelImportConsumer(Integer maxRows, Integer maxRecordsPerTransaction, String focusedSheet, Integer focusedRow, List<String> inactiveSheets) {
         this.maxRows = maxRows;
         this.maxRecordsPerTransaction = maxRecordsPerTransaction;
         this.focusedSheet = focusedSheet;
